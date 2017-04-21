40b63e9e21833fdc4024e62bc447c9eff12fb819
mongardini <joshmongardini@Joshs-MacBook-Pro.local>
2017-04-21 09:40:19 -0400

    Making it better

---
+++ config.js
@@ -0,0 +1,149 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+// Documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: process.env.HEROKU_URL,
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Mailgun',
+                auth: {
+                    user: process.env.MAILGUN_SMTP_LOGIN,
+                    pass: process.env.MAILGUN_SMTP_PASSWORD
+                }
+            }
+        },
+        storage: {
+            active: 'ghost-s3',
+            'ghost-s3': {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN,
+                bucket: process.env.AWS_S3_BUCKET,
+                region: process.env.AWS_REGION,
+                pathPrefix: process.env.AWS_S3_PATH
+            }
+        },
+        database: {
+            client: 'postgres',
+            connection: process.env.DATABASE_URL,
+            debug: false
+        },
+        server: {
+            host: '0.0.0.0',
+            port: process.env.PORT || 2368
+        }
+    },
+
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blogs published URL.
+        url: 'http://localhost:2368',
+
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing MySQL
+    // Used by Travis - Automated testing run through GitHub
+    'testing-mysql': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing pg
+    // Used by Travis - Automated testing run through GitHub
+    'testing-pg': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    }
+};
+
+// Export config
+module.exports = config;
